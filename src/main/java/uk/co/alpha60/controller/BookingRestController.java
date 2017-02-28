b11bf86adbef548c76fe0502dfefa005c4823c07
phlebas <tjc@alpha-60.co.uk>
2017-02-28 10:52:38 +0000

    Use GSON for creating test requests.

--- src/main/java/uk/co/alpha60/controller/BookingRestController.java
+++ src/main/java/uk/co/alpha60/controller/BookingRestController.java
@@ -13,92 +13,95 @@ import java.util.List;
 @RequestMapping("/bookings")
 public class BookingRestController {
 
-    private final BookingRepository bookingRepository;
-    private final CustomerRepository customerRepository;
-    private final RoomRepository roomRepository;
+   private final BookingRepository bookingRepository;
+   private final CustomerRepository customerRepository;
+   private final RoomRepository roomRepository;
 
-    @Autowired
-    public BookingRestController(BookingRepository bookingRepository,
-                                 CustomerRepository customerRepository,
-                                 RoomRepository roomRepository) {
-        this.bookingRepository = bookingRepository;
-        this.customerRepository = customerRepository;
-        this.roomRepository = roomRepository;
-    }
+   @Autowired
+   public BookingRestController(BookingRepository bookingRepository,
+                                CustomerRepository customerRepository,
+                                RoomRepository roomRepository) {
+      this.bookingRepository = bookingRepository;
+      this.customerRepository = customerRepository;
+      this.roomRepository = roomRepository;
+   }
 
-    /**
-     * Retrieves any bookings for a specific room ID
-     * @param roomId the room Id
-     * @return a list of bookings
-     */
-    @RequestMapping(path="/room/{roomId}", method = RequestMethod.GET)
-    Collection<Booking> getBookingsForRoom(@PathVariable Long roomId) {
-        validateRoom(roomId);
-        return bookingRepository.findByRoomId(roomId);
-    }
+   /**
+    * Retrieves any bookings for a specific room ID
+    *
+    * @param roomId the room Id
+    * @return a list of bookings
+    */
+   @RequestMapping(path = "/room/{roomId}", method = RequestMethod.GET)
+   Collection<Booking> getBookingsForRoom(@PathVariable Long roomId) {
+      validateRoom(roomId);
+      return bookingRepository.findByRoomId(roomId);
+   }
 
-    /**
-     * Retrieves any bookings for a specific customer ID
-     * @param customerId the customer Id
-     * @return a list of bookings
-     */
-    @RequestMapping(path = "/customer/{customerId}", method = RequestMethod.GET)
-    Collection<Booking> getBookingsForCustomer(@PathVariable Long customerId) {
-        validateCustomerId(customerId);
-        return bookingRepository.findByCustomerId(customerId);
-    }
+   /**
+    * Retrieves any bookings for a specific customer ID
+    *
+    * @param customerId the customer Id
+    * @return a list of bookings
+    */
+   @RequestMapping(path = "/customer/{customerId}", method = RequestMethod.GET)
+   Collection<Booking> getBookingsForCustomer(@PathVariable Long customerId) {
+      validateCustomerId(customerId);
+      return bookingRepository.findByCustomerId(customerId);
+   }
 
-    /**
-     * Determines the availability of a specific room during a given date range
-     * @param roomId the room Id
-     * @param checkIn checkin date
-     * @param checkOut checkout date
-     * @return True iff a the room is available between these dates.
-     */
-    @RequestMapping(path = "/available/{roomId}/{checkIn}/{checkOut}", method = RequestMethod.GET)
-    public Boolean isRoomAvailable(@PathVariable Long roomId,
-                                   @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkIn,
-                                   @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkOut) {
+   /**
+    * Determines the availability of a specific room during a given date range
+    *
+    * @param roomId   the room Id
+    * @param checkIn  checkin date
+    * @param checkOut checkout date
+    * @return True iff a the room is available between these dates.
+    */
+   @RequestMapping(path = "/available/{roomId}/{checkIn}/{checkOut}", method = RequestMethod.GET)
+   public Boolean isRoomAvailable(@PathVariable Long roomId,
+                                  @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkIn,
+                                  @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkOut) {
 
-        validateRoom(roomId);
-        validateDates(checkIn, checkOut);
-        return roomHasNoBookings(roomId, checkIn, checkOut);
-    }
+      validateRoom(roomId);
+      validateDates(checkIn, checkOut);
+      return roomHasNoBookings(roomId, checkIn, checkOut);
+   }
 
-    private Boolean roomHasNoBookings(Long roomId, LocalDate checkIn, LocalDate checkOut) {
-        List<Booking> bookings = bookingRepository.getBookings(roomId, checkIn, checkOut);
-        return bookings.isEmpty();
-    }
+   private Boolean roomHasNoBookings(Long roomId, LocalDate checkIn, LocalDate checkOut) {
+      List<Booking> bookings = bookingRepository.getBookings(roomId, checkIn, checkOut);
+      return bookings.isEmpty();
+   }
 
-    /**
-     * Makes a booking for a room.
-     * @param booking the booking object
-     * @return the Booking object created.
-     */
-    @RequestMapping(path="/book", method= RequestMethod.POST)
-    public Booking makeBooking(@RequestBody Booking booking) {
+   /**
+    * Makes a booking for a room.
+    *
+    * @param booking the booking object
+    * @return the Booking object created.
+    */
+   @RequestMapping(path = "/book", method = RequestMethod.POST)
+   public Booking makeBooking(@RequestBody Booking booking) {
 
-        validateDates(booking.getCheckIn(), booking.getCheckOut());
-        validateRoom(booking.getId());
-        validateCustomerId(booking.getCustomer().getId());
-        return bookingRepository.save(booking);
+      validateDates(booking.getCheckIn(), booking.getCheckOut());
+      validateRoom(booking.getRoom().getId());
+      validateCustomerId(booking.getCustomer().getId());
+      return bookingRepository.save(booking);
+   }
 
-    }
+   private void validateDates(LocalDate checkIn, LocalDate checkOut) {
+      if (checkOut.isBefore(checkIn)) {
+         throw new InvalidBookingDatesException("Checkout date cannot be before checkin date.");
+      }
+   }
 
-    private void validateDates(LocalDate checkIn, LocalDate checkOut) {
-        if (checkOut.isBefore(checkIn)) {
-            throw new InvalidBookingDatesException("Checkout date cannot be before checkin date.");
-        }
-    }
+   private void validateRoom(Long roomId) throws RoomNotFoundException {
+      roomRepository.findById(roomId).orElseThrow(
+            () -> new RoomNotFoundException(roomId));
+   }
 
-    private void validateRoom(Long roomId) throws RoomNotFoundException {
-        roomRepository.findById(roomId).orElseThrow(
-                () -> new RoomNotFoundException(roomId));
-    }
-
-    private Customer validateCustomerId(Long customerId) throws RoomNotFoundException {
-        return customerRepository.findById(customerId).orElseThrow(
-                () -> new CustomerNotFoundException(customerId));
-    }
+   private Customer validateCustomerId(Long customerId) throws RoomNotFoundException {
+      return customerRepository.findById(customerId).orElseThrow(
+            () -> new CustomerNotFoundException(customerId));
+   }
 
 }
--- src/test/java/uk/co/alpha60/controller/BookingRestControllerTest.java
+++ src/test/java/uk/co/alpha60/controller/BookingRestControllerTest.java
@@ -1,7 +1,12 @@
 package uk.co.alpha60.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +14,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import uk.co.alpha60.HotelApplication;
-import uk.co.alpha60.model.*;
+import uk.co.alpha60.model.Booking;
+import uk.co.alpha60.model.BookingRepository;
+import uk.co.alpha60.model.Customer;
+import uk.co.alpha60.model.CustomerRepository;
+import uk.co.alpha60.model.Room;
+import uk.co.alpha60.model.RoomRepository;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import static java.time.format.DateTimeFormatter.ISO_DATE;
@@ -27,7 +38,9 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
@@ -35,133 +48,151 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @WebAppConfiguration
 public class BookingRestControllerTest {
 
-    private MockMvc mockMvc;
+   private MockMvc mockMvc;
 
-    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+   private HttpMessageConverter mappingJackson2HttpMessageConverter;
 
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
+   private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+         MediaType.APPLICATION_JSON.getSubtype(),
+         Charset.forName("utf8"));
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+   private Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
 
-    @Autowired
-    private BookingRepository bookingRepository;
+   @Autowired
+   private WebApplicationContext webApplicationContext;
 
-    @Autowired
-    private CustomerRepository customerRepository;
+   @Autowired
+   private BookingRepository bookingRepository;
 
-    @Autowired
-    private RoomRepository roomRepository;
-    private Booking booking1;
-    private Customer customer1;
-    private Room room1;
+   @Autowired
+   private CustomerRepository customerRepository;
 
-    @Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
+   @Autowired
+   private RoomRepository roomRepository;
+   private Booking booking1;
+   private Customer customer1;
+   private Room room1;
 
-        mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
-                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
-                .findAny()
-                .orElse(null);
+   @Autowired
+   void setConverters(HttpMessageConverter<?>[] converters) {
 
-        assertNotNull("The JSON message converter must not be null", this.mappingJackson2HttpMessageConverter);
-    }
+      mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+            .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+            .findAny()
+            .orElse(null);
 
-    @Before
-    public void setup() throws Exception {
-        mockMvc = webAppContextSetup(webApplicationContext).build();
+      assertNotNull("The JSON message converter must not be null", this.mappingJackson2HttpMessageConverter);
+   }
 
-        bookingRepository.deleteAllInBatch();
-        customerRepository.deleteAllInBatch();
-        roomRepository.deleteAllInBatch();
+   @Before
+   public void setup() throws Exception {
+      mockMvc = webAppContextSetup(webApplicationContext).build();
 
-        customer1 = customerRepository.save(new Customer(1L, "customer1"));
+      bookingRepository.deleteAllInBatch();
+      customerRepository.deleteAllInBatch();
+      roomRepository.deleteAllInBatch();
 
-        room1 = new Room(1L, 1, 100.0f);
-        room1 = roomRepository.save(room1);
-        Room room2 = new Room(2L, 1, 100.0f);
-        room2 = roomRepository.save(room2);
-        Room room3 = new Room(3L, 2, 200.0f);
-        room3 = roomRepository.save(room3);
-        Room room4 = new Room(4L, 2, 200.0f);
-        room4 = roomRepository.save(room4);
-        Room room5 = new Room(5L, 2, 200.0f);
-        room5 = roomRepository.save(room5);
+      customer1 = customerRepository.save(new Customer(1L, "customer1"));
 
-        LocalDate checkin = LocalDate.now();
-        LocalDate checkout = LocalDate.now().plusDays(3);
-        booking1 = bookingRepository.save(new Booking(1L, room1, customer1, checkin, checkout));
+      room1 = new Room(1L, 1, 100.0f);
+      room1 = roomRepository.save(room1);
+      Room room2 = new Room(2L, 1, 100.0f);
+      room2 = roomRepository.save(room2);
+      Room room3 = new Room(3L, 2, 200.0f);
+      room3 = roomRepository.save(room3);
+      Room room4 = new Room(4L, 2, 200.0f);
+      room4 = roomRepository.save(room4);
+      Room room5 = new Room(5L, 2, 200.0f);
+      room5 = roomRepository.save(room5);
 
-    }
+      LocalDate checkin = LocalDate.now();
+      LocalDate checkout = LocalDate.now().plusDays(3);
+      booking1 = bookingRepository.save(new Booking(1L, room1, customer1, checkin, checkout));
 
-    @Test
-    public void getBookingsForRoom() throws Exception {
-        mockMvc.perform(get("/bookings/room/" + room1.getId())
-                .contentType(contentType))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$[0].id", is(booking1.getId().intValue())))
-                .andExpect(jsonPath("$[0].checkOut", is(booking1.getCheckOut().format(ISO_DATE))))
-                .andExpect(jsonPath("$[0].checkIn", is(booking1.getCheckIn().format(ISO_DATE))));
-    }
+   }
 
-    @Test
-    public void checkIsRoomBooked() throws Exception {
-        String urlTemplate = "/bookings/available/" + room1.getId()
-                + "/" + booking1.getCheckIn().format(ISO_DATE)
-                + "/" + booking1.getCheckOut().format(ISO_DATE);
-        mockMvc.perform(get(urlTemplate)
-                .contentType(contentType))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType));
-    }
+   @Test
+   public void getBookingsForRoom() throws Exception {
+      mockMvc.perform(get("/bookings/room/" + room1.getId())
+            .contentType(contentType))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$[0].id", is(booking1.getId().intValue())))
+            .andExpect(jsonPath("$[0].checkOut", is(booking1.getCheckOut().format(ISO_DATE))))
+            .andExpect(jsonPath("$[0].checkIn", is(booking1.getCheckIn().format(ISO_DATE))));
+   }
 
-    @Test
-    @Ignore("JSON representation of booking does not include customer, room or id fields.")
-    public void postBookingForRoom() throws Exception {
-        String urlTemplate = "/bookings/book";
-        LocalDate checkIn = LocalDate.now().plusDays(10);
-        Booking booking = new Booking(2L, room1, customer1, checkIn, checkIn.plusDays(12));
+   @Test
+   public void checkIsRoomBooked() throws Exception {
+      String urlTemplate = "/bookings/available/" + room1.getId()
+            + "/" + booking1.getCheckIn().format(ISO_DATE)
+            + "/" + booking1.getCheckOut().format(ISO_DATE);
+      mockMvc.perform(get(urlTemplate)
+            .contentType(contentType))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType));
+   }
 
-        String json = json(booking);
+   @Test
+   public void postBookingForRoom() throws Exception {
+      String urlTemplate = "/bookings/book";
+      LocalDate checkIn = LocalDate.now().plusDays(10);
+      Booking booking = new Booking(2L, room1, customer1, checkIn, checkIn.plusDays(12));
 
-        mockMvc.perform(post(urlTemplate)
-                .content(json)
-                .contentType(contentType))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType));
-    }
+      String json = gson.toJson(booking);
 
-    @Test
-    public void findSingleBookingForCustomer() throws Exception {
-        mockMvc.perform(get("/bookings/customer/" + customer1.getId())
-                .contentType(contentType))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$[0].id", is(booking1.getId().intValue())))
-                .andExpect(jsonPath("$[0].checkOut", is(booking1.getCheckOut().format(ISO_DATE))))
-                .andExpect(jsonPath("$[0].checkIn", is(booking1.getCheckIn().format(ISO_DATE))));
-    }
+      mockMvc.perform(post(urlTemplate)
+            .content(json)
+            .contentType(contentType))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$.customer.id", is(booking.getCustomer().getId().intValue())))
+            .andExpect(jsonPath("$.room.id", is(booking.getRoom().getId().intValue())))
+            .andExpect(jsonPath("$.checkIn", is(booking.getCheckIn().format(ISO_DATE))))
+            .andExpect(jsonPath("$.checkOut", is(booking.getCheckOut().format(ISO_DATE))));
+   }
 
-    @Test
-    public void customerNotFound() throws Exception {
-        mockMvc.perform(get("/bookings/customer/0")
-                .contentType(contentType))
-                .andExpect(status().isNotFound());
-    }
+   @Test
+   public void findSingleBookingForCustomer() throws Exception {
+      mockMvc.perform(get("/bookings/customer/" + customer1.getId())
+            .contentType(contentType))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$[0].id", is(booking1.getId().intValue())))
+            .andExpect(jsonPath("$[0].checkOut", is(booking1.getCheckOut().format(ISO_DATE))))
+            .andExpect(jsonPath("$[0].checkIn", is(booking1.getCheckIn().format(ISO_DATE))));
+   }
 
-    @Test
-    public void roomNotFound() throws Exception {
-        mockMvc.perform(get("/bookings/room/0")
-                .contentType(contentType))
-                .andExpect(status().isNotFound());
-    }
+   @Test
+   public void customerNotFound() throws Exception {
+      mockMvc.perform(get("/bookings/customer/0")
+            .contentType(contentType))
+            .andExpect(status().isNotFound());
+   }
 
-    private String json(Object o) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
-    }
+   @Test
+   public void roomNotFound() throws Exception {
+      mockMvc.perform(get("/bookings/room/0")
+            .contentType(contentType))
+            .andExpect(status().isNotFound());
+   }
+
+   @Test
+   public void testJSONWriter() throws IOException {
+      LocalDateAdapter localDateAdapter = new LocalDateAdapter();
+      Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, localDateAdapter).create();
+
+      LocalDate checkIn = LocalDate.now().plusDays(10);
+      Booking booking = new Booking(2L, room1, customer1, checkIn, checkIn.plusDays(12));
+
+      String json = gson.toJson(booking);
+      assertNotNull(json);
+   }
+
+   class LocalDateAdapter implements JsonSerializer<LocalDate> {
+
+      public JsonElement serialize(LocalDate date, Type typeOfSrc, JsonSerializationContext context) {
+         return new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE)); // "yyyy-mm-dd"
+      }
+   }
 }
